f98c4196b4516f5f29d02b676aa9abcaf4188e74
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-07 09:13:13 -0700

    test: add attributes test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe Item do
 
       expect(item1).to be_a Item
     end
+
+    it 'has a name and price' do 
+      item1 = Item.new({name: 'Peach', price: "$0.75"})
+      
+      expect(item1.name).to eq('Peach')
+      expect(item1.price).to eq(0.75)
+    end
   end  
 end
