8ec15c47d14f4bc262e96860a94c22dbb5b5f957
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-07 11:20:17 -0700

    test: create overstocked items test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -120,6 +120,30 @@ RSpec.describe Market do
       expect(market.total_inventory.keys.first).to eq(item1)
       expect(market.total_inventory.values.first[:quantity]).to eq(100)
     end
+  end
+
+  describe '#overstocked_items' do
+    it 'can give items that are overstocked' do
+      market = Market.new('South Pearl Street Farmers Market') 
+      vendor1 = Vendor.new('Rocky Mountain Fresh')
+      vendor2 = Vendor.new('Ba-Nom-a-Nom') 
+      vendor3 = Vendor.new('Palisade Peach Shack') 
+      item1 = Item.new({name: 'Peach', price: '$0.75'})
+      item2 = Item.new({name: 'Tomato', price: '$0.50'})
+      item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+      item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
+
+      market.add_vendor(vendor1) 
+      market.add_vendor(vendor2) 
+      market.add_vendor(vendor3)
+      vendor1.stock(item1, 35)  
+      vendor1.stock(item2, 7)   
+      vendor2.stock(item4, 50) 
+      vendor2.stock(item3, 25)
+      vendor3.stock(item1, 65) 
+
+      expect(market.overstocked_items).to eq([item1])
+    end
 
   end
 end
