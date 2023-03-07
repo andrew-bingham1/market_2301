419ce5262ffc26e942b1581b0743ac8a30e1c4b6
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-07 09:48:38 -0700

    test: create vendor names test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -30,6 +30,19 @@ RSpec.describe Market do
       expect(market.vendors).to eq([vendor1,vendor2])
     end
 
+  describe '#vendor_names' do 
+    it 'can give all vendor names' do 
+      market = Market.new("South Pearl Street Farmers Market") 
+      vendor1 = Vendor.new("Rocky Mountain Fresh")
+      vendor2 = Vendor.new("Ba-Nom-a-Nom") 
+      vendor3 = Vendor.new("Palisade Peach Shack") 
+      market.add_vendor(vendor1) 
+      market.add_vendor(vendor2) 
+      market.add_vendor(vendor3)
+
+      expect(market.vendor_names).to eq(["South Pearl Street Farmers Market", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
   # describe '#vendors_that_sell' do 
   #   it 'can give a list of vendors that sell '
   # end
