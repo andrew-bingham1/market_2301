d6ec00ca830be683de417c97028358c47e1f9016
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-07 11:15:59 -0700

    feat: create total inventory method with helpers

--- lib/market.rb
+++ lib/market.rb
@@ -37,4 +37,40 @@ class Market
     end
     raw_list.flatten.sort.uniq
   end
+
+  def total_number_of_item(item)
+    total = 0 
+    vendors.each do |vendor| 
+      total += vendor.check_stock(item)
+    end
+    total
+  end
+
+  def object_list
+    raw_list = []
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, _|
+        raw_list << item
+      end
+    end
+    raw_list
+  end
+
+  def item_inventory(item)
+    item_hash = {
+      quantity: total_number_of_item(item),
+      vendors: vendors_that_sell(item)
+    }
+  end
+
+  def total_inventory
+    market_inventory = Hash.new
+    object_list.each do |item|
+      market_inventory[item] = item_inventory(item)
+    end
+    market_inventory
+  end
+
+  
+
 end
