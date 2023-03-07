cabd8f960e716d952dd7a2bc173f5c5472e456d4
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-07 11:50:47 -0700

    test: add total number of item helper test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -141,9 +141,40 @@ RSpec.describe Market do
       vendor2.stock(item4, 50) 
       vendor2.stock(item3, 25)
       vendor3.stock(item1, 65) 
-      
+
       expect(market.overstocked_items).to eq([item1])
     end
+  end
 
+  describe '#helper methods' do 
+    it 'can give total number of item' do 
+      market = Market.new('South Pearl Street Farmers Market') 
+      vendor1 = Vendor.new('Rocky Mountain Fresh')
+      vendor2 = Vendor.new('Ba-Nom-a-Nom') 
+      vendor3 = Vendor.new('Palisade Peach Shack') 
+      item1 = Item.new({name: 'Peach', price: '$0.75'})
+      item2 = Item.new({name: 'Tomato', price: '$0.50'})
+      item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+      item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
+
+      market.add_vendor(vendor1) 
+      market.add_vendor(vendor2) 
+      market.add_vendor(vendor3)
+      vendor1.stock(item1, 35)  
+      vendor1.stock(item2, 7)   
+      vendor2.stock(item4, 50) 
+      vendor2.stock(item3, 25)
+      vendor3.stock(item1, 65)
+
+      expect(market.total_number_of_item(item1)).to eq(100)
+    end
+
+    it 'can give list of items' do 
+
+    end
+
+    it 'can create item hash' do 
+
+    end
   end
 end
